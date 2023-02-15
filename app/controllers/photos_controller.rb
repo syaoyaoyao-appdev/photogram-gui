31a02854bb2835c732bb89e688b28bf1fbe0c1de
syaoyaoyao <syao0@chicagobooth.edu>
2023-02-15 12:14:21 +0000

    wip

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,25 @@
+class PhotosController < ApplicationController
+  def index
+    matching_photos = Photo.all
+
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+    render({ :template => "photo_templates/index.html.erb" })
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show.html.erb" })
+  end
+
+  def bali
+    url_id = params.fetch("toast_id")
+    matching_photos = Photo.where({ :id => url_id })
+    the_photo = matching_photos.at(0)
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index" })
 
+  get("/users", { :controller => "users", :action => "index" })
 
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+
+  get("/photos", { :controller => "photos", :action => "index" })
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+
+  get("delete_photo/:toast_id", { :controller => "photos", :action => "bali" })
 end
